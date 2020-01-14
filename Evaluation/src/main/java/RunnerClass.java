3e90e592ea1216eab30fdc0cda066a2f2735c798
Ajaypal Singh <31472828+xxajayxx@users.noreply.github.com>
2020-01-14 12:13:15 +0530

    Update RunnerClass.java

--- Evaluation/src/main/java/RunnerClass.java
+++ Evaluation/src/main/java/RunnerClass.java
@@ -2,7 +2,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 	@RunWith(Cucumber.class)
-	@CucumberOptions(features="C:\\test\\Evaluation\\Features\\Features.feature",plugin="pretty", glue="(default package)")
+	@CucumberOptions(features="C:\\test\\Evaluation\\Features\\Features.feature",plugin="pretty", glue="(default package)",plugin="json:target/cucumber.json")
 	public class RunnerClass{
 		
 		
