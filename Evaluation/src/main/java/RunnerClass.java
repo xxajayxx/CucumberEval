105a7ae41b1251bd63479f32ca7471ad65e881eb
Ajaypal Singh <31472828+xxajayxx@users.noreply.github.com>
2020-01-14 11:24:45 +0530

    Add files via upload

---
+++ Evaluation/src/main/java/RunnerClass.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+	@RunWith(Cucumber.class)
+	@CucumberOptions(features="C:\\test\\Evaluation\\src\\main\\java\\Eval.java",plugin="pretty", glue="(default package)")
+	public class RunnerClass{
+		
+		
+		
+	}
+
+
